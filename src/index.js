981998e7529c6641c6c6aa3eb92980f204c76f8f
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2019-04-30 09:50:36 +0900

    redux-thunkの適用

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import { render } from "react-dom";
 
 import App from "./App";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
 import { Provider } from "react-redux";
 
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const middleWares = [thunk];
+
+const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 const rootElement = document.getElementById("root");
 render(
@@ -17,8 +21,3 @@ render(
   </Provider>,
   rootElement
 );
-
-import giphyAPI from "./APIs/giphyAPI";
-giphyAPI("cat").then(res => {
-  console.log(res.data);
-});
